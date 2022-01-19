43f97012a7ffa2d55d9e2c63fef2dfdcb6ca3d00
joaquin osorio vazquez <kemalmoto.e5@gmail.com>
2022-01-18 23:45:46 -0600

    first commit

---
+++ content.php
@@ -0,0 +1,53 @@
+
+<body>
+
+<h1>hola soy el contenido de esta pagina</h1>
+<div class="titulo_boton">
+   <p>Información adicional</p> 
+  <a style='cursor: pointer;' onClick="muestra_oculta('contenido')" title="" class="boton1">   Mostrar / Ocultar</a>
+</div>
+<div class="texto" id="contenido">
+<center>
+<p>hola soy contenido de texto</p>
+
+
+</center>
+
+
+</div>
+
+<div class="">
+<button>hola soy un boton</button>
+
+</div> 
+    <br>
+    <a style='cursor: help;' onClick="saludar()" title="s" class="boton1"> DAME CLICK </a>
+<br>
+<div>
+<script>
+document.write('Hola');
+</script>
+
+<script>
+document.write(+screen.width);
+</script>
+<script>
+document.write('Ancho de la pantalla: '+screen.width+' pixeles');
+
+</script>
+
+<?php
+//aqui van todos los scripts de javascript
+include 'funciones.js';
+
+?>
+<script>
+    salto()
+    prueba();
+    salto()
+    saludar(" joaquin ")
+    salto()
+    saludar(" max el perro")
+    </script>
+</div>
+</body>
---
+++ funciones.js
@@ -0,0 +1,14 @@
+<script>
+function salto(){
+    document.write("<br>")
+}
+function prueba(){
+
+    document.write("mensaje deprueba desde funciones js")
+
+}
+function saludar(nombre)
+{
+    document.write("hola"+ nombre)
+}
+</script>
