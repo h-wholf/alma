4b60dd7f776bc54b9b5ec41c8f1dc171333a0223
h-wholf <kemalmoto.e5@gmail.com>
2022-03-19 15:11:15 -0600

    ya funciona agregar y mostrar

--- consulta.php
+++ consulta.php
@@ -14,11 +14,6 @@ echo ("<br>");
 echo("<br>");
 
 
-$results = $bd->query('SELECT n_conpleto FROM acta');
-while ($row = $results->fetchArray()) {
-    
-    foreach ($row as $no) {
-    echo "¿Le gusta el $no?\n";
-}
-}
+
+echo (shell_exec("echo SELECT *from acta|sqlite3 prueba1.db"))
 ?>
